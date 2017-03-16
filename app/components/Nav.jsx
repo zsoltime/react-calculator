876821420e3fc16bd12a9137353684b9c78d1e6b
Zsolt Meszaros <zsolt@sowad.com>
2017-03-16 11:39:17 +0100

    Add top menu

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
 const Nav = () => (
-  <div>
-    <IndexLink to="/">Home</IndexLink>
-    <Link to="/about">About</Link>
-  </div>
+  <ul className="menu">
+    <li className="menu__item">
+      <IndexLink
+        to="/"
+        className="menu__link"
+        activeClassName="menu__link--active"
+      >Home</IndexLink>
+    </li>
+    <li className="menu__item">
+      <Link
+        to="/about"
+        className="menu__link"
+        activeClassName="menu__link--active"
+      >About</Link>
+    </li>
+    <li className="menu__item">
+      <a className="menu__link" href="#">More apps</a>
+    </li>
+  </ul>
 );
 
 export default Nav;
