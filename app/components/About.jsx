ca83e8b84b22ec0dc7796ca1035130da48d9ff9f
Zsolt Meszaros <zsolt@sowad.com>
2017-04-06 14:51:05 +0100

    Add about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    <h1>About</h1>
-    <p>This is a simple React calculator for FCC.</p>
+  <div className="page">
+    <h1 className="page__title">About</h1>
+    <p>This is my React calculator for Free Code Camp&apos;s first advanced front end projects.</p>
+    <h2 className="page__subtitle">Tools Used</h2>
+    <ul>
+      <li>React.js</li>
+      <li>React router</li>
+      <li>Webpack module bundler</li>
+      <li>Babel.js compiler</li>
+      <li>ESLint linter with Airbnb&apos;s config</li>
+      <li>Sass with PostCSS Autoprefixer</li>
+      <li>Karma test runner</li>
+      <li>Mocha test framework</li>
+      <li>Expect assertion library</li>
+    </ul>
   </div>
 );
 
